b0ef4e481c9731812920cb02c2768bddf4b4b9b3
saufiroja <saufiroja08@gmail.com>
2022-12-03 13:20:23 +0700

    feat: add client streaming

--- greet/client/main.go
+++ greet/client/main.go
@@ -5,6 +5,7 @@ import (
 	"fmt"
 	"io"
 	"log"
+	"time"
 
 	"google.golang.org/grpc"
 	"google.golang.org/grpc/credentials/insecure"
@@ -24,8 +25,9 @@ func main() {
 
 	c := pb.NewGreetServiceClient(conn)
 
-	doGreet(c) // <--- call the function
-	doGreetManyTimes(c)
+	// doGreet(c) // <--- call the function
+	// doGreetManyTimes(c)
+	doLongGreet(c)
 
 	log.Printf("Connected to %s", addr)
 }
@@ -71,4 +73,32 @@ func doGreetManyTimes(c pb.GreetServiceClient) {
 
 		log.Printf("GreetManyTimes: %s\n", msg.Result)
 	}
+}
+
+func doLongGreet(c pb.GreetServiceClient) {
+	log.Println("do longgreet was invoked")
+
+	req := []*pb.GreetRequest{
+		{FirstName: "saufi"},
+		{FirstName: "anang"},
+		{FirstName: "ikura"},
+	}
+
+	stream, err := c.LongGreet(context.Background())
+	if err != nil {
+		log.Fatalf("Error while calling LongGreet %v\n", err)
+	}
+
+	for _, v := range req {
+		log.Printf("Sending req: %v\n", req)
+		stream.Send(v)
+		time.Sleep(1 * time.Second)
+	}
+
+	res, err := stream.CloseAndRecv()
+	if err != nil {
+		log.Fatalf("Error while receiving response from LongGreet: %v\n", err)
+	}
+
+	log.Printf("LongGreet: %s\n", res.Result)
 }
--- greet/server/main.go
+++ greet/server/main.go
@@ -3,6 +3,7 @@ package main
 import (
 	"context"
 	"fmt"
+	"io"
 	"log"
 	"net"
 
@@ -52,4 +53,27 @@ func (s *Server) GreetManyTimes(req *pb.GreetRequest, stream pb.GreetService_Gre
 	}
 
 	return nil
+}
+
+func (s *Server) LongGreet(stream pb.GreetService_LongGreetServer) error {
+	log.Printf("LongGreet function was invoked with a streaming request\n")
+
+	res := ""
+
+	for {
+		req, err := stream.Recv()
+		if err == io.EOF {
+			return stream.SendAndClose(&pb.GreetResponse{
+				Result: res,
+			})
+		}
+
+		if err != nil {
+			log.Fatalf("Error while reading client stream: %v\n", err)
+		}
+
+		log.Printf("Receiving %v\n", req)
+
+		res += fmt.Sprintf("Hello %s\n", req.FirstName)
+	}
 }
